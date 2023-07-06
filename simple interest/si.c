af6e9db20bde37b98573968f37bb85e2539988be
Ankita Urkude <ankitaurkude4742@gmail.com>
2023-07-06 21:29:34 +0530

    intitial commit

---
+++ simple interest/si.c
@@ -0,0 +1,9 @@
+// program to calculate simple interest for given input
+#include<stdio.h>
+#include<conio.h>
+int main() {
+    int p = 100, r = 5, t =1;
+    float si = (p*r*t)/100;
+    printf("simple interest for given input is %f", si);
+    return 0;
+}
